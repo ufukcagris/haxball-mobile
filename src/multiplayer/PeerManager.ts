2bc0307de6ce2918c0f718c29703a3cbfdac39bb
ufukcagris <trkycs@gmail.com>
2026-04-03 14:22:51 +0300

    multiplayer: add TURN servers and optimize WebRTC config for NAT traversal

--- src/multiplayer/PeerManager.ts
+++ src/multiplayer/PeerManager.ts
@@ -38,13 +38,34 @@ export class PeerManager {
     
     this.peer = new Peer({
       secure: isSecure,
+      debug: 1,
       config: {
         iceServers: [
           { urls: 'stun:stun.l.google.com:19302' },
           { urls: 'stun:stun1.l.google.com:19302' },
           { urls: 'stun:stun2.l.google.com:19302' },
+          { urls: 'stun:stun3.l.google.com:19302' },
+          { urls: 'stun:stun4.l.google.com:19302' },
           { urls: 'stun:stun.services.mozilla.com' },
-        ]
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          }
+        ],
+        iceTransportPolicy: 'all',
+        sdpSemantics: 'unified-plan',
+        iceCandidatePoolSize: 10
       }
     });
 
